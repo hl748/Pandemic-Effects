a188b226bcea512d65545d19277b8faae73351d3
Connor Cote <cotec4@gmail.com>
2020-07-21 21:49:33 -0400

    Adding NYTimes AJAX request beginning Javascript for AJAX

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems, options);
-  });
-
-  // Or with jQuery
-
-  $(document).ready(function(){
+// Loading the Drop-down list
+$(document).ready(function () {
     $('select').formSelect();
-  });
+});
+
+var shortTerm = document.getElementsByName("shortTerm");
+var queryURL;
+var keywordSearch = $("#keywordSearch")[0].value;
+
+function NYTimesSearch() {
+    var APIKey = "2dUYhsd7NHElbbIY9bgav2GCAlGSin97";
+    var titleArray = [];
+    var beginDate;
+
+    if (shortTerm[0].checked === true) {
+        beginDate = 20200201;
+        queryURL = "https:api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keywordSearch + "&api-key=" + APIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+    }
+    else {
+        beginDate = moment().format("YYYYMMDD") - 10000;
+        queryURL = "https:api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keywordSearch + "&api-key=" + APIKey;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+        })
+    }
+}
+
+$("#searchBtn").on("click", NYTimesSearch);
