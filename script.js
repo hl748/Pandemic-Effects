043727ca5f96620ad87d3fa58aed242e416d22ef
Connor Cote <cotec4@gmail.com>
2020-07-23 22:19:36 -0400

    Updating JS and HTML

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 // Loading the Drop-down list and the Carouselgut
 $(document).ready(function () {
-  $('select').formSelect();
-  $('.carousel').carousel();
+    $('select').formSelect();
+    $('.carousel').carousel();
 });
 
 $(".current-date").text(moment().format("LLL"));
 
 // Global variable
-var shortTerm = document.getElementsByName("shortTerm");
 var keywordSearch = $("#keywordSearch")[0];
 
 // new global variables
@@ -15,87 +14,82 @@ var title
 var word
 var titleArray = []
 var removeWords = ['the', 'a', 'an', 'some', 'saw', 'time', 'really', 'today', 'dont', 'do', 'of', 'is', 'are', 'going', 'to', 'got', 'didnt', 'cant', 'can', 'will', 'finally', 'going', 'new', 'wait', 'think', 'just', 'see', 'one', 'still', 'might', 'shall', 'in', 'for', 'and', "",
-  'be', 'as', 'arent', 'how', 'with', 'its', keywordSearch.value.toLowerCase()]
-var displayObj = [{}];
-
-console.log(displayObj)
+    'be', 'as', 'arent', 'how', 'with', 'its', keywordSearch.value.toLowerCase()]
+var displayObj = {};
 
 // function to remove words
 function RemoveWords() {
-  title = title.replace(/[^a-zA-Z ]/g, "").toLowerCase()
-  word = title.split(" ")
-  word = word.filter(function (x) {
-    return !removeWords.includes(x);
-  });
-  titleArray = titleArray.concat(word);
+    title = title.replace(/[^a-zA-Z ]/g, "").toLowerCase()
+    word = title.split(" ")
+    word = word.filter(function (x) {
+        return !removeWords.includes(x);
+    });
+    titleArray = titleArray.concat(word);
 }
 
 // function to sort and count number of duplicated words
 function sortWords() {
-  titleArray.sort();
-  var current = null;
-  var cnt = 0;
-  for (var i = 0; i <= titleArray.length; i++) {
-    if (titleArray[i] != current) {
-      if (cnt > 0) {
-        localStorage.setItem(cnt, current)
-      }
-      current = titleArray[i];
-      cnt = 1;
-    } else {
-      
-      localStorage.setItem(cnt, current)
-      cnt++;
+    titleArray.sort();
+    var current = null;
+    var cnt = 0;
+    localStorage.setItem("titleArray",JSON.stringify(titleArray));
+    for (var i = 0; i <= titleArray.length; i++) {
+        if (titleArray[i] != current) {
+            if (cnt > 0) {
+                displayObj[titleArray[i]] = cnt;
+            }
+            current = titleArray[i];
+            cnt = 1;
+        } else {
+            cnt++;
+        }
     }
-  }
-  if (cnt > 0) {
-    localStorage.setItem(cnt, current)
-  }
+    var maxValue = 0;
+    for (var [key, value] of Object.entries(displayObj)) {
+        if (`${value}` > maxValue) {
+            maxValue = `${value}`; 
+        }
+        console.log(`${key}: ${value}`)
+    }
 
+    if (cnt > 0) {
+    }
 }
 
-function largestCounts () {
-  localStorage.getItem()
-}
+// Find all the values with max values 
 
 // The Guardian function
 function GuardianSearch() {
-  var guardianAPI = "fac02636-ec64-432c-80e9-88d7553d783c"
-  var beginDate;
-  if (shortTerm[0].checked === true) {
-    beginDate = '2020-02-01'
-    var guardianURL = "https://content.guardianapis.com/search?q=" + keywordSearch.value + "&from-date=2020-02-01&api-key=" + guardianAPI;
-    console.log(keywordSearch)
-    $.ajax({
-      url: guardianURL,
-      method: "GET"
-    }).then(function (response) {
-      for (var i = 0; i < 10; i++) {
-        title = response.response.results[i].webTitle
-
-        RemoveWords()
-      }
-      sortWords()
-      largestCounts()
-      console.log(titleArray)
-      console.log(displayObj)
-    })
-  }
-
-  else {
-    beginDate = moment().format("YYYYMMDD") - 10000
-    var guardianURL = "https://content.guardianapis.com/search?q=" + keywordSearch.value + "&from-date=" + beginDate + "&api-key=" + guardianAPI;
-    console.log(keywordSearch)
-    $.ajax({
-      url: guardianURL,
-      method: "GET"
-    }).then(function (response) {
-      for (var i = 0; i < 10; i++) {
-        console.log(response.response.results[i].webTitle)
-      }
+    var guardianAPI = "fac02636-ec64-432c-80e9-88d7553d783c"
+    var beginDate;
+    if ($("#shortTerm")[0].checked === true) {
+        beginDate = '2020-02-01'
+        var guardianURL = "https://content.guardianapis.com/search?q=" + keywordSearch.value + "&from-date=2020-02-01&api-key=" + guardianAPI;
+        $.ajax({
+            url: guardianURL,
+            method: "GET"
+        }).then(function (response) {
+            for (var i = 0; i < 10; i++) {
+                title = response.response.results[i].webTitle;
+                RemoveWords();
+            }
+            sortWords();
+        })
     }
-    )
-  }
+
+    else {
+        beginDate = moment().format("YYYYMMDD") - 10000
+        var guardianURL = "https://content.guardianapis.com/search?q=" + keywordSearch.value + "&from-date=" + beginDate + "&api-key=" + guardianAPI;
+        console.log(keywordSearch)
+        $.ajax({
+            url: guardianURL,
+            method: "GET"
+        }).then(function (response) {
+            for (var i = 0; i < 10; i++) {
+            }
+        }
+        )
+    }
 }
 
 // function NYTimesSearch() {
@@ -111,7 +105,10 @@ function GuardianSearch() {
 //             url: NYTimesURL,
 //             method: "GET"
 //         }).then(function (response) {
-//             console.log(response);
+//             title = response.response.docs[i].main;
+//             RemoveWords();
+//             sortWords();
+//             largestCount();
 //         })
 //     }
 //     else {
