0a2d331055405310d17ad8a4db53cd3faf29eee5
macpro <haeseungsung@MacBook-Pro.local>
2020-07-23 22:00:49 -0400

    I edited Connor's code to pull NYT titles

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
 $(".current-date").text(moment().format("LLL"));
 
 // Global variable
-var shortTerm = document.getElementsByName("shortTerm");
 var keywordSearch = $("#keywordSearch")[0];
 
 // new global variables
@@ -22,12 +21,14 @@ console.log(displayObj)
 
 // function to remove words
 function RemoveWords() {
-  title = title.replace(/[^a-zA-Z ]/g, "").toLowerCase()
-  word = title.split(" ")
-  word = word.filter(function (x) {
-    return !removeWords.includes(x);
-  });
-  titleArray = titleArray.concat(word);
+  if (title !== null) {
+    title = title.replace(/[^a-zA-Z ]/g, "").toLowerCase()
+    word = title.split(" ")
+    word = word.filter(function (x) {
+      return !removeWords.includes(x);
+    });
+    titleArray = titleArray.concat(word);
+  }
 }
 
 // function to sort and count number of duplicated words
@@ -38,93 +39,80 @@ function sortWords() {
   for (var i = 0; i <= titleArray.length; i++) {
     if (titleArray[i] != current) {
       if (cnt > 0) {
-        localStorage.setItem(cnt, current)
+        // localStorage.setItem(cnt, current)
       }
       current = titleArray[i];
       cnt = 1;
     } else {
-      
-      localStorage.setItem(cnt, current)
+
+      // localStorage.setItem(cnt, current)
       cnt++;
     }
   }
   if (cnt > 0) {
-    localStorage.setItem(cnt, current)
+    // localStorage.setItem(cnt, current)
   }
 
 }
 
-function largestCounts () {
-  localStorage.getItem()
-}
+// function largestCounts() {
+// localStorage.getItem()
+// }
 
 // The Guardian function
 function GuardianSearch() {
   var guardianAPI = "fac02636-ec64-432c-80e9-88d7553d783c"
   var beginDate;
-  if (shortTerm[0].checked === true) {
-    beginDate = '2020-02-01'
+  if ($("#shortTerm")[0].checked === true) {
     var guardianURL = "https://content.guardianapis.com/search?q=" + keywordSearch.value + "&from-date=2020-02-01&api-key=" + guardianAPI;
-    console.log(keywordSearch)
-    $.ajax({
-      url: guardianURL,
-      method: "GET"
-    }).then(function (response) {
-      for (var i = 0; i < 10; i++) {
-        title = response.response.results[i].webTitle
 
-        RemoveWords()
-      }
-      sortWords()
-      largestCounts()
-      console.log(titleArray)
-      console.log(displayObj)
-    })
-  }
-
-  else {
+  } else {
     beginDate = moment().format("YYYYMMDD") - 10000
     var guardianURL = "https://content.guardianapis.com/search?q=" + keywordSearch.value + "&from-date=" + beginDate + "&api-key=" + guardianAPI;
-    console.log(keywordSearch)
-    $.ajax({
-      url: guardianURL,
-      method: "GET"
-    }).then(function (response) {
-      for (var i = 0; i < 10; i++) {
-        console.log(response.response.results[i].webTitle)
-      }
-    }
-    )
   }
+
+  $.ajax({
+    url: guardianURL,
+    method: "GET"
+  }).then(function (response) {
+    for (var i = 0; i < 10; i++) {
+      title = response.response.results[i].webTitle
+      RemoveWords()
+    }
+    sortWords()
+    // largestCounts()
+    console.log(titleArray)
+    console.log(displayObj)
+  })
 }
 
-// function NYTimesSearch() {
-//     var APIKey = "2dUYhsd7NHElbbIY9bgav2GCAlGSin97";
-//     var titleArray = [];
-//     var NYTimesURL;
-//     var beginDate;
 
-//     if (shortTerm[0].checked === true) {
-//         beginDate = 20200201;
-//         NYTimesURL = "https:api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keywordSearch + "&api-key=" + APIKey;
-//         $.ajax({
-//             url: NYTimesURL,
-//             method: "GET"
-//         }).then(function (response) {
-//             console.log(response);
-//         })
-//     }
-//     else {
-//         beginDate = moment().format("YYYYMMDD") - 10000;
-//         queryURL = "https:api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keywordSearch + "&api-key=" + APIKey;
-//         $.ajax({
-//             url: NYTimesURL,
-//             method: "GET"
-//         }).then(function (response) {
-//             console.log(response);
-//         })
-//     }
-// }
+function NYTimesSearch() {
+  var APIKey = "2dUYhsd7NHElbbIY9bgav2GCAlGSin97";
+  var NYTimesURL;
+  var beginDate;
 
-$("#searchBtn").on("click", GuardianSearch)
-// $("#searchBtn").on("click", NYTimesSearch);
+  if ($("#shortTerm")[0].checked === true) {
+    beginDate = 20200201;
+    NYTimesURL = "https:api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keywordSearch.value + "&api-key=" + APIKey;
+  } else {
+    beginDate = moment().format("YYYYMMDD") - 10000;
+    queryURL = "https:api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keywordSearch.value + "&api-key=" + APIKey;
+  }
+  $.ajax({
+    url: NYTimesURL,
+    method: "GET"
+  }).then(function (response) {
+    for (var i = 0; i < 10; i++) {
+      title = response.response.docs[i].headline.print_headline
+      RemoveWords()
+    }
+    sortWords()
+    // largestCounts()
+    console.log(titleArray)
+    console.log(displayObj)
+  })
+}
+
+// $("#searchBtn").on("click", GuardianSearch)
+$("#searchBtn").on("click", NYTimesSearch);
